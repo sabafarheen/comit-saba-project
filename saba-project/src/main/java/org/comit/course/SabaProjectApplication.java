e4b0ec44175bca51309588485a1062ad920f60fe
Saba Farheen <sabafarheen2003@gmail.com>
2022-08-13 12:39:06 -0700

    first commit

---
+++ saba-project/src/main/java/org/comit/course/SabaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabaProjectApplication.class, args);
+	}
+
+}
